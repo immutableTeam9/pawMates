f842bead0d5fad43e56fb9cd11525ba2b3683b4b
JellyBear97 <olxloha@gamil.com>
2023-06-29 20:47:26 +0900

    Core : merge 완료

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Header from './components/Header';
 import Home from './pages/Home';
+import DetailComments from './components/DetailComments';
+import DetailPost from './components/DetailPost';
 
 export default function App() {
   return (
-
     <>
       <Header />
       <Home />
+      <div>
+        <DetailPost />
+        <DetailComments />
+      </div>
     </>
   );
 }
