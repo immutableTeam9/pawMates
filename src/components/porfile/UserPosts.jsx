4f8d1e6aca1ffb574ffcd933874768c076a02aa0
sumin <ysm4260@gmail.com>
2023-06-30 18:00:48 +0900

    Feat : 반려동물 정보 불러오기

--- src/components/porfile/UserPosts.jsx
+++ src/components/porfile/UserPosts.jsx
@@ -8,29 +8,55 @@ const UserPosts = () => {
   const { id } = useParams();
 
   const [posts, setPosts] = useState([]);
-  const fetchData = async () => {
-    const q = query(collection(db, 'posts'));
+  const [petInfos, setPetInfos] = useState([]);
+  const fetchData = async (collectionName) => {
+    const q = query(collection(db, collectionName));
     const querySnapshot = await getDocs(q);
-    const initialPosts = [];
+    const initialArr = [];
     querySnapshot.forEach((doc) => {
-      initialPosts.push({ id: doc.id, ...doc.data() });
+      initialArr.push({ id: doc.id, ...doc.data() });
     });
-    setPosts(initialPosts);
+    switch (collectionName) {
+      case 'posts':
+        setPosts(initialArr);
+        return;
+      case 'petInfo':
+        setPetInfos(initialArr);
+      default:
+        return;
+    }
   };
 
   useEffect(() => {
-    fetchData();
+    fetchData('posts');
+    fetchData('petInfo');
   }, []);
 
+  const userPetInfo = petInfos.find((petInfo) => petInfo.ownerId === id);
   return (
-    <div>
-      <h2>작성한 게시물</h2>
-      {posts
-        .filter((post) => post.userId === id)
-        .map((post) => {
-          return <PostItem key={post.id} post={post}></PostItem>;
-        })}
-    </div>
+    <>
+      <div>
+        <h2>반려동물 정보</h2>
+        {userPetInfo ? (
+          <div>
+            <p>{userPetInfo.name}</p>
+            <p>{userPetInfo.type}</p>
+            <p>{userPetInfo.age}</p>
+            <p>{userPetInfo.gender}</p>
+          </div>
+        ) : (
+          <div>등록한 반려동물 정보가 없습니다.</div>
+        )}
+      </div>
+      <div>
+        <h2>작성한 게시물</h2>
+        {posts
+          .filter((post) => post.userId === id)
+          .map((post) => {
+            return <PostItem key={post.id} post={post}></PostItem>;
+          })}
+      </div>
+    </>
   );
 };
 
